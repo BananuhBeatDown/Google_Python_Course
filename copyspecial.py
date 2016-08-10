be770425dad00a576067a6b6a840b6e1180285c0
Matt Green <GrizAdams@users.noreply.github.com>
2016-08-10 12:46:00 -0400

    Add files via upload

--- copyspecial.py
+++ copyspecial.py
@@ -6,11 +6,11 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
-import sys
 import re
 import os
 import shutil
-import commands
+from zipfile import *
+
 
 """Copy Special exercise
 """
@@ -22,54 +22,25 @@ import commands
 def getSpecialPaths(destdir):    
     filenames = os.listdir(destdir)
     regex = re.compile("__\w+__")
-    x = [os.path.abspath(os.path.join(destdir, filename)) for filename in filenames for m in [regex.search(filename)] if m]
-    return x
+    paths = [os.path.abspath(os.path.join(destdir, filename)) for filename in filenames for m in [regex.search(filename)] if m]
+    return paths
 
 
 def copyTo(path, destdir):
-    paths = getSpecialPaths(path)    
-    for p in paths:
-        if os.path.isdir(destdir):
-            shutil.copy(p, destdir)
-        else: 
-            os.makedirs(destdir) # create all directories, raise an error if it already exists
+    if not os.path.exists(destdir):
+        os.makedirs(destdir)
+    for p in path:
             shutil.copy(p, destdir)
 
             
-def zipTo(paths, zippath):
-   
-   
-
-def main():
-  # This basic command line argument parsing code is provided.
-  # Add code to call your functions below.
-
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
-  if not args:
-    print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
-    sys.exit(1)
-
-  # todir and tozip are either set from command line
-  # or left as the empty string.
-  # The args array is left just containing the dirs.
-  todir = ''
-  if args[0] == '--todir':
-    todir = args[1]
-    del args[0:2]
-
-  tozip = ''
-  if args[0] == '--tozip':
-    tozip = args[1]
-    del args[0:2]
-
-  if len(args) == 0:
-    print "error: must specify one or more dirs"
-    sys.exit(1)
-
-  # +++your code here+++
-  # Call your functions
-  
-if __name__ == "__main__":
-  main()
+def zipTo(path, zippath):
+   zf = ZipFile("%s.zip" % (zippath), "w", zipfile.ZIP_DEFLATED)
+   absPath = os.path.abspath(path)
+   for dirname, subdirs, files in os.walk(path):
+       for filename in files:
+           absname = os.path.abspath(os.path.join(dirname, filename))
+           arcname = absname[len(absPath) + 1:]
+           print 'zipping %s as %s' % (os.path.join(dirname, filename), arcname)
+           zf.write(absname, arcname)
+   zf.close
+            
